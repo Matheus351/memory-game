d1568f38096cee2a4d54eb45cc201b37bc998494
Matheus Valença <matheusniklaus@gmail.com>
2022-07-01 11:22:01 -0300

    adicionando função para validar input

---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('.login_input')
+const button = document.querySelector('.login_button')
+
+const validateInput = ({target})=>{
+    if(target.value.length > 2){
+        button.removeAttribute('disabled')
+        return
+    }
+    button.setAttribute('disabled','')
+}
+
+input.addEventListener('input',validateInput)
+
